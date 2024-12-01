9da9d68c04427a2d502494de5b98bfc22aefeb6a
Péter Hanich <hanipet@gmail.com>
2024-12-01 22:40:13 +0100

    Simplify names for sources

--- ETL/Extract/Input/extract.py
+++ ETL/Extract/Input/extract.py
@@ -22,8 +22,8 @@ if not os.path.exists(extract_path):
     os.makedirs(extract_path)
 
 # Download latest version
-kaggle.api.dataset_download_files("parasrupani/coffee-distribution-across-94-counties", path=extract_path + "/coffee-distribution-across-94-counties/", unzip=True)
-kaggle.api.dataset_download_files("fatihb/coffee-quality-data-cqi", path=extract_path + "/coffee-quality-data-cqi/", unzip=True)
-kaggle.api.dataset_download_files('schmoyote/coffee-reviews-dataset', path=extract_path + "/coffee-reviews-dataset/", unzip=True)
-kaggle.api.dataset_download_files("ihelon/coffee-sales", path=extract_path + "/coffee-sales/", unzip=True)
-kaggle.api.dataset_download_files("ahmedmohamedibrahim1/coffee-shop-sales-dataset", path=extract_path + "/coffee-shop-sales-dataset/", unzip=True)
+kaggle.api.dataset_download_files("parasrupani/coffee-distribution-across-94-counties", path=extract_path + "/distribution/", unzip=True)
+kaggle.api.dataset_download_files("fatihb/coffee-quality-data-cqi", path=extract_path + "/quality/", unzip=True)
+kaggle.api.dataset_download_files('schmoyote/coffee-reviews-dataset', path=extract_path + "/reviews/", unzip=True)
+kaggle.api.dataset_download_files("ihelon/coffee-sales", path=extract_path + "/sales/", unzip=True)
+kaggle.api.dataset_download_files("ahmedmohamedibrahim1/coffee-shop-sales-dataset", path=extract_path + "/shop-sales/", unzip=True)
