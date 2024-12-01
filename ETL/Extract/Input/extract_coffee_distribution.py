e72785e92b0d63f6c37f792ed3aa30af185aae9c
Péter Hanich <hanipet@gmail.com>
2024-12-01 18:23:39 +0100

    Create extract job with ploomber

---
+++ ETL/Extract/Input/extract_coffee_distribution.py
@@ -0,0 +1,26 @@
+# Ploomber API
+# %% tags=["parameters"]
+upstream = None
+product = None
+data_path = None
+
+# %%
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+#Import kaggle credentials from .env file
+KAGGLE_USERNAME = os.getenv("KAGGLE_USERNAME")
+KAGGLE_KEY= os.getenv("KAGGLE_KEY")
+
+import kaggle
+
+kaggle.api.authenticate()
+
+# Create the directory if it does not exist
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
+
+# Download latest version
+kaggle.api.dataset_download_files("parasrupani/coffee-distribution-across-94-counties", path=data_path+"/coffee-distribution-across-94-counties/", unzip=True)
