1505ce8ab4d1eee28e02f972391775dd67b1c3a8
Péter Hanich <hanipet@gmail.com>
2024-12-04 10:49:16 +0100

    Complete sales cleaning

--- ETL/Transform/Input/clean_sales.py
+++ ETL/Transform/Input/clean_sales.py
@@ -9,19 +9,78 @@ transform_path = None
 # %%
 import pandas as pd
 
+
 def remove_unnecessary_columns(dataframe: pandas.DataFrame, columns: [str]) -> pandas.DataFrame:
     dataframe.drop(columns=columns, inplace=True)
     return dataframe
 
-def save_to_csv(df: pandas.DataFrame, output_path: str) -> None:
-    df.to_csv(output_path, index=False)
+
+def convert_currency_to_usd(dataframe: pandas.DataFrame, currency_column: str,
+                            exchange_rate: float) -> pandas.DataFrame:
+    # Change according to the exchange rate and round to 2 decimal places
+    dataframe[currency_column] = dataframe[currency_column] * exchange_rate
+    dataframe[currency_column] = dataframe[currency_column].round(2)
+    return dataframe
+
+
+def remove_milliseconds(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+    dataframe['datetime'] = pd.to_datetime(dataframe['datetime']).dt.floor('s')
+    return dataframe
+
+
+def add_product_category(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+    # Add a new column to the dataframe called product_category
+    # If the coffee_name is Hot Chocolate set the product_category to 'Drinking Chocolate' else Coffee
+    dataframe['product_category'] = 'Coffee'
+    dataframe.loc[dataframe['coffee_name'] == 'Hot Chocolate', 'product_category'] = 'Drinking Chocolate'
+    return dataframe
+
+
+def add_product_details(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+    # Add a new column to the dataframe called product_detail
+    # Set it to '-' everywhere
+    dataframe['product_detail'] = '-'
+    return dataframe
+
+
+def add_quantity(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+    # Add a new column to the dataframe called transaction_qty
+    # Set it to 1 everywhere
+    dataframe['transaction_qty'] = 1
+    return dataframe
+
+
+def rename_columns(dataframe: pandas.DataFrame, columns: {str: str}) -> pandas.DataFrame:
+    dataframe.rename(columns=columns, inplace=True)
+    return dataframe
+
+
+def reorganize_columns(dataframe: pandas.DataFrame, columns: [str]) -> pandas.DataFrame:
+    return dataframe[columns]
+
+
+def save_to_csv(dataframe: pandas.DataFrame, output_path: str) -> None:
+    dataframe.to_csv(output_path, index=False)
+
 
 def load_csv(input_path: str) -> pandas.DataFrame:
     return pd.read_csv(input_path)
 
-columns_to_remove = ['card']
+
+columns_to_remove = ['card', 'cash_type']
 file_path = extract_path + '/sales/index.csv'
+UAH_TO_USD = 0.024  # 2024 UAH to USD
 
 df = load_csv(file_path)
-remove_unnecessary_columns(df,columns_to_remove)
+df = remove_unnecessary_columns(df, columns_to_remove)
+df = convert_currency_to_usd(df, 'money', UAH_TO_USD)
+df = remove_milliseconds(df)
+df = add_product_category(df)
+df = add_product_details(df)
+df = add_quantity(df)
+df = rename_columns(df, {'coffee_name': 'product_type', 'money': 'unit_price', 'datetime': 'transaction_time',
+                         'date': 'transaction_date'})
+df = reorganize_columns(df,
+                        ['transaction_date', 'transaction_time', 'transaction_qty', 'unit_price', 'product_category',
+                         'product_type', 'product_detail'])
 save_to_csv(df, product['data'])
--- ETL/Transform/Input/clean_shop_sales.py
+++ ETL/Transform/Input/clean_shop_sales.py
@@ -9,19 +9,85 @@ transform_path = None
 # %%
 import pandas as pd
 
-def load_xlsx(input_path: str,sheet_name:str) -> pandas.DataFrame:
-    return pd.read_excel(input_path,sheet_name=sheet_name)
+
+def load_xlsx(input_path: str, sheet_name: str) -> pandas.DataFrame:
+    return pd.read_excel(input_path, sheet_name=sheet_name)
+
 
 def remove_unnecessary_columns(dataframe: pandas.DataFrame, columns: [str]) -> pandas.DataFrame:
     dataframe.drop(columns=columns, inplace=True)
     return dataframe
 
-def save_to_csv(df: pandas.DataFrame, output_path: str) -> None:
-    df.to_csv(output_path, index=False)
 
-columns_to_remove = ['store_id','store_location','product_id']
+def remove_bakery(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+    return dataframe[dataframe['product_category'] != 'Bakery']
+
+
+def clean_coffee_products(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+    # Where product_type is Barista Espresso, set the product_type
+    # to the product_details first word and make the detail_empty
+    espresso_rows = dataframe.loc[dataframe['product_type'] == 'Barista Espresso']
+    espresso_rows['product_type'] = espresso_rows['product_detail'].str.split(' ').str[0]
+    espresso_rows['product_detail'] = '-'
+    # Where the product_type contains the word 'brewed' and the product_category is 'Coffee'
+    # set the type to Brew Coffee and
+    # set the product_detail to the product_details first word
+    brew_rows = dataframe.loc[(dataframe['product_type'].str.contains('brewed', case=False)) & (
+            dataframe['product_category'] == 'Coffee')]
+    brew_rows['product_detail'] = brew_rows['product_detail'].str.split(' ').str[0]
+    brew_rows['product_type'] = 'Brew Coffee'
+    # Remove detail on Drip Coffee
+    drip_rows = dataframe.loc[dataframe['product_type'] == 'Drip coffee']
+    drip_rows['product_type'] = 'Drip Coffee'
+    drip_rows['product_detail'] = '-'
+    dataframe.update(espresso_rows)
+    dataframe.update(brew_rows)
+    dataframe.update(drip_rows)
+    return dataframe
+
+
+def clean_teas(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+    # In every teas type colum keep the second word and capitalize the first letter and
+    # set the product_detail to '-'
+    tea_rows = dataframe.loc[dataframe['product_category'] == 'Tea']
+    tea_rows['product_type'] = tea_rows['product_type'].str.split(' ').str[1].str.capitalize()
+    tea_rows['product_detail'] = '-'
+    dataframe.update(tea_rows)
+    return dataframe
+
+
+def clean_hot_chocolate(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+    # For every Drinking Chocolate Category set the detail to '-' and the type to 'Hot Chocolate'
+    hot_chocolate_rows = dataframe.loc[dataframe['product_category'] == 'Drinking Chocolate']
+    hot_chocolate_rows['product_type'] = 'Hot Chocolate'
+    hot_chocolate_rows['product_detail'] = '-'
+    dataframe.update(hot_chocolate_rows)
+    return dataframe
+
+
+def change_time_to_datetime(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+    # Get the transaction_date and transaction_time columns and merge them into
+    # the time column and convert it to datetime
+    # Concatenate the transaction_date and transaction_time columns row-wise
+    dataframe['transaction_time'] = dataframe['transaction_date'].astype(str) + ' ' + dataframe[
+        'transaction_time'].astype(str)
+    dataframe['transaction_time'] = pd.to_datetime(dataframe['transaction_time'])
+    return dataframe
+
+
+def save_to_csv(dataframe: pandas.DataFrame, output_path: str) -> None:
+    dataframe.to_csv(output_path, index=False)
+
+
+columns_to_remove = ['store_id', 'store_location', 'product_id', 'Revenue', 'Month', 'Month.1', 'Weekday', 'Weekday.1',
+                     'Hour']
 file_path = extract_path + '/shop-sales/Coffee Shop Sales.xlsx'
 
-df = load_xlsx(file_path,'Transactions')
-remove_unnecessary_columns(df,columns_to_remove)
+df = load_xlsx(file_path, 'Transactions')
+df = remove_unnecessary_columns(df, columns_to_remove)
+df = remove_bakery(df)
+df = clean_coffee_products(df)
+df = clean_teas(df)
+df = clean_hot_chocolate(df)
+df = change_time_to_datetime(df)
 save_to_csv(df, product['data'])
